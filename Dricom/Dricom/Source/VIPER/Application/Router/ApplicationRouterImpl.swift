3b240d525560927323f0a7ee6d298505ee4824fc
Ivan Bondar <ibondar@avito.ru>
2017-02-24 13:28:25 +0300

    Fix presentation style for splash screen Use system font on splash screen

--- Dricom/Dricom/Source/VIPER/Application/Router/ApplicationRouterImpl.swift
+++ Dricom/Dricom/Source/VIPER/Application/Router/ApplicationRouterImpl.swift
@@ -10,6 +10,8 @@ final class ApplicationRouterImpl: BaseRouter, ApplicationRouter {
         let assembly = assemblyFactory.appStarterAssembly()
         let module = assembly.module(disposeBag: disposeBag, mainPageModule: mainPageModule)
         
+        module.viewController.modalTransitionStyle = .crossDissolve
+        module.viewController.modalPresentationStyle = .fullScreen
         viewController?.present(module.viewController, animated: false) {
             completion?(module.applicationLaunchHandler)
         }
